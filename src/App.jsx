50730d39f65f868abceff8cb0106a3cd562828e0
vdsou <vdsouza@outlook.com>
2021-09-23 10:16:00 -0300

    remove unecessary files for the project, turned components .js into .jsx. And also now each component will be in class format.

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+  render() {
+    return <h1>hello, world!</h1>;
+  }
+}
+
+export default App;
